92fb53d7e4629d195cd82a926d810928fc635d6f
Carlos N. Trejo <14352052+CarlosTrejo2308@users.noreply.github.com>
2024-11-11 09:39:32 -0600

    Finish course project

---
+++ src/main/generated/com/platzi/market/persistence/mapper/CategoryMapperImpl.java
@@ -0,0 +1,49 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persistence.entity.Categoria;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-11-11T09:28:29-0600",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 17.0.8.1 (Eclipse Adoptium)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public Category toCategory(Categoria categoria) {
+        if ( categoria == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        if ( categoria.getIdCategoria() != null ) {
+            category.setCategoryId( categoria.getIdCategoria() );
+        }
+        category.setCategory( categoria.getDescripcion() );
+        if ( categoria.getEstado() != null ) {
+            category.setActive( categoria.getEstado() );
+        }
+
+        return category;
+    }
+
+    @Override
+    public Categoria toCategoria(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        Categoria categoria = new Categoria();
+
+        categoria.setIdCategoria( category.getCategoryId() );
+        categoria.setDescripcion( category.getCategory() );
+        categoria.setEstado( category.isActive() );
+
+        return categoria;
+    }
+}
